34f5c2b1560a4687836d46a337c1b41ecce1d135
Artem Kuznetsov <at0m616@yandex.ru>
2022-05-31 10:52:14 +0500

    Initial commit

---
+++ weathernow/src/main/java/com/project/weathernow/WeathernowApplication.java
@@ -0,0 +1,13 @@
+package com.project.weathernow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeathernowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeathernowApplication.class, args);
+	}
+
+}
